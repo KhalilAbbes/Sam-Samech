6863640e864c30e5a91bf3ffaa2bd4245b1dc923
MaryemBess <47980211+MaryemBess@users.noreply.github.com>
2019-04-10 15:09:03 +0100

    Delete background.h

--- background.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-typedef struct{Sdl_surface *image;Sdl_Rect pos;Mix_Music *son;}background;
-void initialiser_background(background *b);
-void clean_background(background *b,SDL_Surface *screen);
